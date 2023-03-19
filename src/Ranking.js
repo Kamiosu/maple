f9a090b78cf3ca419de1f9384bb5691c76d1e0b1
Ohm Ray <ohmray020@gmail.com>
2023-03-19 00:56:07 -0400

    kinda working ranking

--- src/Ranking.js
+++ src/Ranking.js
@@ -4,10 +4,15 @@ import Papa from 'papaparse';
 import React, { useState, useEffect } from 'react';
 
 function Card(itemData) {
-    console.log(itemData.data)
     return (
         <div className="flex flex-col bg-[rgb(254,249,255)] rounded-[20px] relative mt-9 mx-0 mb-0">
             <div className="bg-[rgb(150,3,26)] rounded-[20px] w-[14.21%] relative grow min-h-[121px] mt-0 mr-0 mb-0 ml-[85.79%]" />
+            {/* Add data from itemData to div */}
+            <div className="lg:w-[92.34%] md:w-[94.14%] sm:w-[95.54%] xs:w-[96.62%] xxs:w-[97.44%] tn:w-[98.07%] flex flex-col w-[90.04%] relative grow mt-[50px] mx-auto mb-[74px]">
+                <h1 className="lg:text-[56px] lg:text-left md:text-[48px] sm:text-[40px] xs:text-[36px] xxs:text-[32px] tn:text-[28px] font-semibold text-[64px] leading-[1.2] font-Inter text-[rgb(150,3,26)] tracking-[0px] w-[20.32%] relative my-0 mx-auto ">
+                    {itemData.data}
+                </h1>
+            </div>
         </div>
     );
 }
