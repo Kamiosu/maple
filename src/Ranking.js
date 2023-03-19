04982905e8f19a4b8c29111a12d20b62677a3e3a
William-04 <74840163+Kamiosu@users.noreply.github.com>
2023-03-19 00:03:33 -0400

    updated

--- src/Ranking.js
+++ src/Ranking.js
@@ -3,14 +3,20 @@ import App from './App';
 
 function Card(name, location, severity) {
     return (
-    <div className="flex flex-col bg-[rgb(254,249,255)] rounded-[20px] relative mt-9 mx-0 mb-0">
-        <div className="bg-[rgb(150,3,26)] rounded-[20px] w-[14.21%] relative grow min-h-[121px] mt-0 mr-0 mb-0 ml-[85.79%]" />
-    </div>
+        <div className="flex flex-col bg-[rgb(254,249,255)] rounded-[20px] relative mt-9 mx-0 mb-0">
+            <div className="bg-[rgb(150,3,26)] rounded-[20px] w-[14.21%] relative grow min-h-[121px] mt-0 mr-0 mb-0 ml-[85.79%]" />
+        </div>
     );
 }
 // Create iterative function 
 
 export default function Ranking(props) {
+    const Item = ({ data }) => {
+        return <div>{data}</div>;
+    };
+
+    const dataArray = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+
     return (
         <div className="ranking flex flex-col bg-[rgb(56,56,81)] relative overflow-hidden">
             <div className="xl:w-[75.6%] lg:w-[88.8%] md:w-[91.36%] sm:w-[93.37%] xs:w-[94.95%] xxs:w-[96.16%] tn:w-[97.09%] flex flex-col w-[81.68%] relative grow mt-6 mx-auto mb-[123px]">
@@ -20,15 +26,19 @@ export default function Ranking(props) {
 
                 <div className="flex flex-col bg-[rgb(217,217,217)] rounded-[30px] relative mt-[38px] mx-0 mb-0 overflow-auto max-h-[600px]">
                     <div className="lg:w-[92.34%] md:w-[94.14%] sm:w-[95.54%] xs:w-[96.62%] xxs:w-[97.44%] tn:w-[98.07%] flex flex-col w-[90.04%] relative grow mt-[50px] mx-auto mb-[74px]">
-                        
-                        <Card/>
-                        <Card/>
-                        <Card/>
-                        <Card/>
-                        <Card/>
-                        <Card/>
-                        <Card/>
-                        <Card/>
+                        {
+                            dataArray.map((itemData, index) => (
+                                <Item key={index} data={itemData} />
+                            ))
+                        }
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
 
                         <div className="flex flex-col bg-[rgb(254,249,255)] rounded-[20px] relative mt-9 mx-0 mb-0">
                             <div className="bg-[rgb(150,3,26)] rounded-[20px] w-[14.21%] relative grow min-h-[121px] mt-0 mr-0 mb-0 ml-[85.79%]" />
